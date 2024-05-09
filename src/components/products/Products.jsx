030af941c553f42754c98a0813eef39e04a56dbc
AshirovSherzod <ashirovsherzod614@gmail.com>
2024-05-09 21:13:58 +0500

    task done

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './products.scss'
+import axios from '../../api'
 
 const Products = () => {
+  const [products, setProducts] = useState(null)
+  const [offset, setOffSet] = useState(1)
+  let perPageCount = 6
+
+  useEffect(()=> {
+    axios.get('/products', {
+      params: {
+        limit: offset * perPageCount
+      }
+    })
+      .then(res => setProducts(res.data.products))
+      .catch(err => console.log(err))
+  }, [offset])
+
+  let productCard = products?.map(el => (
+    <div className="products__card">
+      <div className="products__card__img">
+        <img src={el.images[0]} alt={el.title} />
+      </div>
+      <div className="products__card__desc">
+        <h1>{el.title}</h1>
+        <div className="products__card__cost">
+
+        <p>price: ${el.price}</p>
+        <p>rating: {el.rating}</p>
+        </div>
+      </div>
+    </div>
+  ))
   return (
-    <div>Products</div>
+    <section className='products container'>
+      <h1>Our Latest Products</h1>
+      <div className="products__cards">
+        {productCard}
+      </div>
+      <button onClick={()=> setOffSet(p => p + 1)}>See more</button>
+    </section>
   )
 }
 
--- src/components/watches/Watches.jsx
+++ src/components/watches/Watches.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import './watches.scss'
+import apple from '../../assets/apple-img.png'
+import xiaomi from '../../assets/xiaomi-img.png'
+import bitfit from '../../assets/bitfit-img.png'
 
 const Watches = () => {
+
   return (
-    <div>Watches</div>
+    <section className='watches container'>
+        <div className="watches__card">
+          <div className="watches__apple__img">
+            <img src={apple} alt="" />
+          </div>
+          <div className="watches__apple__desc">
+            <h2>Apple</h2>
+            <p>Apple is one of the <br /> most famous smart <br /> watches providing <br /> company.</p>
+          </div>
+        </div>
+        <div className="watches__card">
+          <div className="watches__apple__img">
+            <img src={xiaomi} alt="" />
+          </div>
+          <div className="watches__apple__desc">
+            <h2>Xiaomi</h2>
+            <p>Xiaomi smart watches <br /> are produced by MI <br /> company.</p>
+          </div>
+        </div>
+        <div className="watches__card">
+          <div className="watches__apple__img">
+            <img src={bitfit  } alt="" />
+          </div>
+          <div className="watches__apple__desc">
+            <h2>BitFit</h2>
+            <p>FitBit smart watches <br /> are best for there <br /> health and fitness <br /> features.</p>
+          </div>
+        </div>
+    </section>
   )
 }
 
